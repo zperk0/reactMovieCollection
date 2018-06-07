b34261d92d96bfc7a1bce03025b02c23c4e0edab
zperk0 <zperkins9@protonmail.com>
2018-06-07 19:30:48 +0900

    Fixed routes for the exact path

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -4,47 +4,68 @@
 /**
  * Created by joewolfgram on 2/8/17.
  */
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Pages from '../components/pages'
-import Navigation from '../components/common/Navigation';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Pages from "../components/pages";
+import Navigation from "../components/common/Navigation";
 
 class NavBar extends React.Component {
-    static contextTypes = {
-        router: React.PropTypes.object.isRequired
-    };
+  static contextTypes = {
+    router: React.PropTypes.object.isRequired
+  };
 
-    render() {
-        browserHistory = this.context.router.history;
-        return (<Navigation logoImgPath="/images/movie.jpg" authenticated={true} menuItems={Pages} router={this.context.router}/>);
-    }
+  render() {
+    browserHistory = this.context.router.history;
+    return (
+      <Navigation
+        logoImgPath="/images/movie.jpg"
+        authenticated={true}
+        menuItems={Pages}
+        router={this.context.router}
+      />
+    );
+  }
 }
 
 function displayRoutes() {
-    let routesArray = [];
-    let routesWithParamsArray = [];
-    for (let componentName in Pages) {
-        if (Pages[componentName].routeParams === true) {
-            routesArray.push(<Route key={componentName} path={Pages[componentName].href} exactly component={Pages[componentName].component} />);
-        }
-        routesArray.push(<Route key={componentName} path={Pages[componentName].href} exactly component={Pages[componentName].component} />);
+  let routesArray = [];
+  let routesWithParamsArray = [];
+  for (let componentName in Pages) {
+    if (Pages[componentName].routeParams === true) {
+      routesArray.push(
+        <Route
+          key={componentName}
+          path={Pages[componentName].href}
+          exact
+          component={Pages[componentName].component}
+        />
+      );
     }
-    return (routesArray.concat(routesWithParamsArray))
+    routesArray.push(
+      <Route
+        key={componentName}
+        path={Pages[componentName].href}
+        exact
+        component={Pages[componentName].component}
+      />
+    );
+  }
+  return routesArray.concat(routesWithParamsArray);
 }
 
-const Routes = (props) => {
-    return (
-        <BrowserRouter {...props}>
-            <div>
-                <Switch>
-                    <Route exactly component={NavBar}/>
-                </Switch>
-                <Switch>
-                    {displayRoutes()}
-                </Switch>
-            </div>
-        </BrowserRouter>
-    );
+const Routes = props => {
+  return (
+    <BrowserRouter {...props}>
+      <div>
+        <Switch>
+          <Route exactly component={NavBar} />
+        </Switch>
+        <Switch>
+          {displayRoutes()}
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export let browserHistory;
